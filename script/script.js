9e0e005332eeff5d5275e19cb8b3a84051bcee59
uruly <rk.uru007@gmail.com>
2020-02-20 13:06:33 +0900

    :rocket: Add appearance scene

--- script/script.js
+++ script/script.js
@@ -30,6 +30,14 @@
      * @type {number}
      */
     let viperY = CANVAS_HEIGHT / 2
+    /**
+     * @type {boolean} - 自機が登場中かどうかを表すフラグ
+     */
+    let isComing = false;
+    /**
+     * @type {number} - 登場演出を開始した際のタイムスタンプ
+     */
+    let comingStart = null;
 
     window.addEventListener('load', () => {
         util = new Canvas2DUtility(document.body.querySelector('#main_canvas'));
@@ -48,11 +56,31 @@
     function initialize() {
         canvas.width = CANVAS_WIDTH;
         canvas.height = CANVAS_HEIGHT;
+
+        isComing = true;
+        comingStart = Date.now();
+        viperY = CANVAS_HEIGHT;
     }
 
     function render() {
+        ctx.globalAlpha = 1.0;
         util.drawRect(0, 0, canvas.width, canvas.height, '#eeeeee');
 
+        // let nowTime = (Date.now() - startTime) / 1000;
+    
+        if (isComing === true) {
+            let justTime = Date.now();
+            let comingTime = (justTime - comingStart) / 1000;
+            viperY = CANVAS_HEIGHT - comingTime * 50;
+            if (viperY <= CANVAS_HEIGHT - 100) {
+                isComing = false;
+                viperY = CANVAS_HEIGHT - 100;
+            }
+
+            if (justTime % 100 < 50) {
+                ctx.globalAlpha = 0.5;
+            }
+        }
         ctx.drawImage(image, viperX, viperY);
 
         requestAnimationFrame(render);
@@ -60,6 +88,7 @@
 
     function eventSetting() {
         window.addEventListener('keydown', (event) => {
+            if (isComing === true) { return; }
             switch(event.key) {
                 case 'ArrowLeft':
                     viperX -= 10;
